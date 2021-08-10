9a527127d8aece27b326230b6777d00a6959c0d2
mayIlearnloopsbrother <77263914+mayIlearnloopsbrother@users.noreply.github.com>
2021-08-10 13:24:16 +0100

    Update add2ip.py

--- add2ip.py
+++ add2ip.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-import socket
+import socket               #importing the socket module
 
-a = input("enter full website name.. e.g. www.abc.com: ")
+a = input("enter full website name.. e.g. www.abc.com: ")     # stores user input in the variable a
 
-print(socket.gethostbyname(str(a))) 
+print(socket.gethostbyname(str(a)))     # printing the variable 'a' to turn it into an ip address by using the 'socket.gethostbyname' class
